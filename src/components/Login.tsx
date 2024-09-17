3d95475af78772bc5b51320c47dd1f1e7aca7ef4
GhostWire619 <lugata619@gmail.com>
2024-09-17 16:14:55 +0300

    dd

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -29,99 +29,108 @@ const LoginPage: React.FC = () => {
   return (
     <div
       style={{
-        maxWidth: "400px",
-        margin: "0 auto",
-        padding: "20px",
-        backgroundColor: "#0d0f0f",
-        borderRadius: "8px",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-        maxHeight: "400px",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div style={{ marginBottom: "15px" }}>
-          <label
-            style={{
-              display: "block",
-              marginBottom: "5px",
-              fontWeight: "bold",
-            }}
-          >
-            Username:
-          </label>
-          <input
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            required
+      <div
+        style={{
+          maxWidth: "400px",
+          margin: "0 auto",
+          padding: "20px",
+          backgroundColor: "#0d0f0f",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+          maxHeight: "400px",
+        }}
+      >
+        <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div style={{ marginBottom: "15px" }}>
+            <label
+              style={{
+                display: "block",
+                marginBottom: "5px",
+                fontWeight: "bold",
+              }}
+            >
+              Username:
+            </label>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+              style={{
+                width: "100%",
+                padding: "8px",
+                borderRadius: "4px",
+                border: "none",
+                boxSizing: "border-box",
+                color: "white",
+                outline: "none",
+              }}
+            />
+          </div>
+          <div style={{ marginBottom: "15px" }}>
+            <label
+              style={{
+                display: "block",
+                marginBottom: "5px",
+                fontWeight: "bold",
+              }}
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              style={{
+                width: "100%",
+                padding: "8px",
+                borderRadius: "4px",
+                border: "none",
+                boxSizing: "border-box",
+                color: "white",
+                outline: "none",
+              }}
+            />
+          </div>
+          <button
+            type="submit"
             style={{
               width: "100%",
-              padding: "8px",
-              borderRadius: "4px",
-              border: "none",
-              boxSizing: "border-box",
+              padding: "10px",
+              backgroundColor: "#007BFF",
               color: "white",
-              outline: "none",
-            }}
-          />
-        </div>
-        <div style={{ marginBottom: "15px" }}>
-          <label
-            style={{
-              display: "block",
-              marginBottom: "5px",
-              fontWeight: "bold",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
             }}
           >
-            Password:
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px",
-              borderRadius: "4px",
-              border: "none",
-              boxSizing: "border-box",
-              color: "white",
-              outline: "none",
-            }}
-          />
-        </div>
-        <button
-          type="submit"
-          style={{
-            width: "100%",
-            padding: "10px",
-            backgroundColor: "#007BFF",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
-          Login
-        </button>
-      </form>
-      {error && (
-        <p style={{ color: "red", marginTop: "15px", textAlign: "center" }}>
-          {error}
+            Login
+          </button>
+        </form>
+        {error && (
+          <p style={{ color: "red", marginTop: "15px", textAlign: "center" }}>
+            {error}
+          </p>
+        )}
+        <p style={{ marginTop: "15px", textAlign: "center" }}>
+          Don’t have an account yet?{" "}
+          <a
+            href="#"
+            onClick={() => setisRegisterd(false)}
+            style={{ color: "#007BFF", textDecoration: "none" }}
+          >
+            Click here
+          </a>
         </p>
-      )}
-      <p style={{ marginTop: "15px", textAlign: "center" }}>
-        Don’t have an account yet?{" "}
-        <a
-          href="#"
-          onClick={() => setisRegisterd(false)}
-          style={{ color: "#007BFF", textDecoration: "none" }}
-        >
-          Click here
-        </a>
-      </p>
+      </div>
     </div>
   );
 };
